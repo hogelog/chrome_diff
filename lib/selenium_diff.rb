b2bea3d6034fa8339e8f199b7f670a4f2c40e4ac
sunao-komuro <sunao-komuro@cookpad.com>
2019-05-11 22:57:37 +0900

    bundle gem selenium_diff

---
+++ lib/selenium_diff.rb
@@ -0,0 +1,6 @@
+require "selenium_diff/version"
+
+module SeleniumDiff
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/selenium_diff/version.rb
@@ -0,0 +1,3 @@
+module SeleniumDiff
+  VERSION = "0.1.0"
+end
