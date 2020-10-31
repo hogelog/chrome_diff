069968d980804ba73e1acc9a014d40f0b5f9aeb4
hogelog <konbu.komuro@gmail.com>
2020-10-31 20:09:55 +0900

    Fuzz percent is floating point number

--- lib/chrome_diff/cli.rb
+++ lib/chrome_diff/cli.rb
@@ -27,7 +27,7 @@ module ChromeDiff
       parser.on("-w [WIDTH]", "--width", "Window width (default 800)") {|v| opts[:width] = v.to_i }
       parser.on("-h [HEIGHT]", "--height", "Window height (default 600)") {|v| opts[:height] = v.to_i }
       parser.on("-q", "--quiet", "Quiet mode") {|v| opts[:quiet] = v }
-      parser.on("--fuzz [FUZZ]", "Fuzz factor percent (default 5%)") {|v| opts[:fuzz] = v.to_i }
+      parser.on("--fuzz [FUZZ]", "Fuzz factor percent (default 5%)") {|v| opts[:fuzz] = v.to_f }
       parser.on("--debug", "Debug mode (default false)") {|v| opts[:debug] = !!v }
       parser.parse(argv)
 
